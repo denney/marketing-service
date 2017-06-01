e5e5b81c36fc9dfdb10f808b883846cdd553b79d
PC <PC@DESKTOP-G41G5AE>
2017-06-01 15:50:06 +0800

    meetingActivities

--- src/main/java/com/wondertek/baiying/marketing/web/rest/ModelContentRuleController.java
+++ src/main/java/com/wondertek/baiying/marketing/web/rest/ModelContentRuleController.java
@@ -1,5 +1,7 @@
 package com.wondertek.baiying.marketing.web.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wondertek.baiying.marketing.domain.ModelContentRule;
+import com.wondertek.baiying.marketing.repository.ModelContentRuleRepository;
 import com.wondertek.baiying.marketing.service.ModelContentRuleService;
 
 /**
@@ -15,22 +18,34 @@ import com.wondertek.baiying.marketing.service.ModelContentRuleService;
  */
 @RestController
 @RequestMapping("/modelContentRule")
-public class ModelContentRuleController {
+public class ModelContentRuleController{
 
 	@Autowired
     private ModelContentRuleService modelContentRuleService;
-
+	private final ModelContentRuleRepository modelContentRuleRepository;
+	public ModelContentRuleController(ModelContentRuleRepository modelContentRuleRepository) {
+        this.modelContentRuleRepository = modelContentRuleRepository;
+    }
 	@RequestMapping("/save")
-	@ResponseBody
     public ResponseEntity<ModelContentRule> save(ModelContentRule modelContentRule){
 		ModelContentRule contentRule =  modelContentRuleService.save(modelContentRule);
 		return new ResponseEntity<>(contentRule,null,HttpStatus.OK);
     }
 	
 	@RequestMapping("/findByNodeIdAndModelId")
-	@ResponseBody
     public ResponseEntity<ModelContentRule> findByNodeIdAndModelId(String nodeId,String modelId){
 		ModelContentRule contentRule = modelContentRuleService.findByNodeIdAndModelId(nodeId, modelId);
 		return new ResponseEntity<>(contentRule,null,HttpStatus.OK);
+    }
+	@RequestMapping("/findAll")
+    public ResponseEntity<List<ModelContentRule>> findAll(){
+		List<ModelContentRule> contentRule = modelContentRuleRepository.findAll();
+		return new ResponseEntity<>(contentRule,null,HttpStatus.OK);
+    }
+	
+	@RequestMapping("/findAllx")
+	public List<ModelContentRule> findAllx(){
+		List<ModelContentRule> contentRule = modelContentRuleRepository.findAll();
+		return contentRule;
     }
 }

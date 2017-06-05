31ab3ae4bc8792cdb29e2256492e50cea3f9daa7
guoxiujuan <18717921955@139.com>
2017-06-05 17:05:50 +0800

    增加注解

--- src/main/java/com/wondertek/baiying/marketing/service/AwardService.java
+++ src/main/java/com/wondertek/baiying/marketing/service/AwardService.java
@@ -2,15 +2,18 @@ package com.wondertek.baiying.marketing.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wondertek.baiying.marketing.repository.AwardRepository;
 
-
+@Service
+@Transactional
 public class AwardService {
 	
-	public final Logger log = LoggerFactory.getLogger(AwardService.class);
+	private final Logger log = LoggerFactory.getLogger(AwardService.class);
 	
-	public final AwardRepository awardRepository;
+	private final AwardRepository awardRepository;
 	
 	public AwardService(AwardRepository awardRepository) {
 		this.awardRepository = awardRepository;

761320bfbc0ba9b0702f26413e4ec3f6e4af9eb6
liqiang <2279566041@qq.com>
2017-06-07 16:41:54 +0800

    奖品

--- src/main/java/com/wondertek/baiying/marketing/repository/AwardRepository.java
+++ src/main/java/com/wondertek/baiying/marketing/repository/AwardRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.wondertek.baiying.marketing.domain.Award;
 
 public interface AwardRepository extends MongoRepository<Award, String> {
+	
+	Award findOneByName(String name);
 
 }

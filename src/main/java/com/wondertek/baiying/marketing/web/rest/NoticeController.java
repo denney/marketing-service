0a6cb65f99d367a0c05a212280712901267bae3f
liuxiaoyan <1181495759@qq.com>
2017-06-12 11:01:20 +0800

    公告controller添加方法

--- src/main/java/com/wondertek/baiying/marketing/web/rest/NoticeController.java
+++ src/main/java/com/wondertek/baiying/marketing/web/rest/NoticeController.java
@@ -17,76 +17,87 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/notice")
 public class NoticeController {
 
-    @Autowired
-    private NoticeService noticeService;
+	@Autowired
+	private NoticeService noticeService;
 
-    /**新增
-     * @param notice
-     * @return
-     */
-    @RequestMapping("/save")
-    public Notice save(Notice notice) {
-        return noticeService.save(notice);
-    }
+	/**
+	 * 新增
+	 * 
+	 * @param notice
+	 * @return
+	 */
+	@RequestMapping("/save")
+	public Notice save(Notice notice) {
+		return noticeService.save(notice);
+	}
 
-    /**  根据id查找一个公告
-     * @param id
-     * @return
-     */
-    @RequestMapping("/findOneById")
-    public Notice findOneByid(Long id) {
-    	Notice resultOfId = noticeService. findOneById(id);
-        return resultOfId;
-    }
+	/**
+	 * 根据id查找一个公告
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/findOneById")
+	public Notice findOneByid(Long id) {
+		Notice resultOfId = noticeService.findOneById(id);
+		return resultOfId;
+	}
 
-    /**分页查询
-     * @param page
-     * @param size
-     * @param direction
-     * @param property
-     * @return
-     */
-    @RequestMapping("/findAllByPaging")
-    public Page<Notice> findAllByPaging(int page, int size, String direction,
+	/**
+	 * 分页查询
+	 * 
+	 * @param page
+	 * @param size
+	 * @param direction
+	 * @param property
+	 * @return
+	 */
+	@RequestMapping("/findAllByPaging")
+	public Page<Notice> findAllByPaging(int page, int size, String direction,
 			String property) {
-    	Page<Notice> resultOfPage=noticeService.findAllByPaging(page, size, direction,
-				property);
-        return resultOfPage;
-    }
+		Page<Notice> resultOfPage = noticeService.findAllByPaging(page, size,
+				direction, property);
+		return resultOfPage;
+	}
 
-    /**根据id删除公告
-     * @param id
-     */
-    @RequestMapping("/delete")
+	/**
+	 * 根据id删除公告
+	 * 
+	 * @param id
+	 */
+	@RequestMapping("/delete")
 	public void deleteById(Long id) {
 		noticeService.deleteById(id);
 	}
-    
-    /**根据所属的appid查询公告
-     * @param appId
-     * @return
-     */
-    @RequestMapping("/findByAppId")
-    public List<Notice> findByAppId(String appId) {
-    	List<Notice> listsOfAppId = noticeService.findByAppId(appId);
-        return listsOfAppId;
-    }
-    
-    /**发布公告
-     * @param id
-     * @return
-     */
-    @RequestMapping("/findOneByUserId")
-    public String publishNotice(Long id){
+
+	/**
+	 * 根据所属的appid查询公告
+	 * 
+	 * @param appId
+	 * @return
+	 */
+	@RequestMapping("/findByAppId")
+	public List<Notice> findByAppId(String appId) {
+		List<Notice> listsOfAppId = noticeService.findByAppId(appId);
+		return listsOfAppId;
+	}
+
+	/**
+	 * 发布公告
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/publishNotice")
+	public String publishNotice(Long id) {
 		Notice notice = noticeService.findOneById(id);
-		if(notice.getOnlineStatus().equals("0")){
+		if (notice.getOnlineStatus().equals("0")) {
 			notice.setOnlineStatus("1");
 			noticeService.save(notice);
 			return "success";
-		}else{
+		} else {
 			return "failure";
 		}
-		
-   }
-}
 
+	}
+}
